24d876108cfbaa9b679d76337d875a8b55151c46
jun <rafajdantas@gmail.com>
2024-06-04 11:37:48 -0300

    implement client side session

--- frontend/src/services/auth/session.js
+++ frontend/src/services/auth/session.js
@@ -1,5 +1,8 @@
 import { authService } from "./authService";
+import React from 'react';
+import { useRouter } from 'next/router';
 
+// SSR
 export function withSession(func) {
   return async (context) => {
     try {
@@ -20,5 +23,55 @@ export function withSession(func) {
         }
       }
     }
+  }
+}
+
+// SSG
+export function useSession() {
+  const [session, setSession] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError]     = React.useState(null);
+
+  React.useEffect(() => {
+    authService.getSession()
+      .then((userSession) => {
+        //console.log(userSession);
+        setSession(userSession);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  
+  return {
+    data: {
+      session,
+    },
+    loading,
+    error,
+  }
+}
+
+export function withSessionHOC(Component) {
+  return function Wrapper(props) {
+    const router = useRouter();
+    const session = useSession();
+    console.log(session);
+
+    if(!session.loading && session.error){
+      console.log("Redireciona para Home");
+      router.push('/?error=401');
+    }
+
+    const modifiedProps = {
+      ...props,
+      session: session.data.session,
+    }
+    return (
+      <Component {...modifiedProps}/>
+    )
   }
 }
